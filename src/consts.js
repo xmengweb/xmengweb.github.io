788725bb171e67653460e9c53bb91a4522621ac6
xmengweb <dd2419247021@163.com>
2023-05-03 23:15:44 +0800

    初始修改

--- src/consts.js
+++ src/consts.js
@@ -1,11 +1,11 @@
-export const SITE_TITLE = `Air Blog`;
+export const SITE_TITLE = `XMeng Air Blog`;
 export const SITE_DESCRIPTION = 'Site Description';
-export const SITE_EMAIL = 'no.sql@qq.com'
-export const SITE_NAME = 'astro.yufengbiji.com';
-export const SITE_URL = "https://astro.yufengbiji.com";
+export const SITE_EMAIL = 'dd2419247021@163.com'
+export const SITE_NAME = 'www.xmengweb.cn';
+export const SITE_URL = "https://www.xmengweb.cn";
 export const SITE_LANG = "zh-CN";
 
 // Author, used only when author is not specified on the page, 
 // has lower priority than the author specified in the md file.
 // 作者信息，仅用在文章没有指定作者的时候，优先显示在 md 文件中指定的作者。
-export const SITE_AUTHOR = "Austin";
+export const SITE_AUTHOR = "XMeng";
